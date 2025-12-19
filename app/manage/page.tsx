3f8d0dc73256016fd8a78ca7ec49fb1371a75dc2
koscpatryk1259-jpg <koscpatryk1259@gmail.com>
2025-12-18 19:44:10 -0500

    Add multi-page website structure

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Navigation from '../components/Navigation';
+import Navigation from '../components/navigation';
 import { useState } from 'react';
 
 export default function ManagePage() {
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import Navigation from './components/Navigation';
+import Navigation from './components/navigation';
 
 export default function Home() {
   const [formData, setFormData] = useState({
